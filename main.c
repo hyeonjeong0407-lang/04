ed1f3bf6902eec7a28b2de601ff056f11e1c8e30
hyeonjeong0407-lang <hyeonjeong0407@gmail.com>
2025-09-25 16:05:53 +0900

    05

--- main.c
+++ main.c
@@ -7,12 +7,12 @@ int main(int argc, char *argv[]) {
 	unsigned int x;
 	int b;
 	printf("input a number : ");
-	scanf("ui", &x);
+	scanf("%ui", &x);
 	
 	for (b=0; x !=0; x>>= 1)
 	{
 		
-	if (x&1)
+	if (x & 1)
 	{
 		b++;
 	}
